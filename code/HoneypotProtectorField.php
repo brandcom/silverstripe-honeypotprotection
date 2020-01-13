084f7c47de8263a5874a5f08130b3af9ce3b8bc6
Julian Bennecker <jb@brandcom.de>
2020-01-13 13:21:58 +0100

    autocomplete off

--- code/HoneypotProtectorField.php
+++ code/HoneypotProtectorField.php
@@ -6,22 +6,19 @@ class HoneypotProtectorField extends TextField
     public function Field($properties = [])
     {
         Requirements::css('silverstripe-honeypotprotection/css/style.css');
+        $this->setAttribute('autocomplete', 'off');
+
         return parent::Field($properties);
     }
 
     public function validate($validator): bool
     {
         if (!empty($this->Value())) {
-            $validator->validationError($this->name, _t('HoneypotProtectorField.INCORRECTSOLUTION', ""), "error");
+            $validator->validationError($this->name, _t('HoneypotProtectorField.INCORRECTSOLUTION', "Spamprotection: Please leave this field empty."), "error");
 
             return false;
         }
 
         return true;
-    }
-
-    public function Title(): string
-    {
-        return 'Phone';
     }
 }
